3cc41c3744ef8e24bc4ff598ad9a6a17a424db65
Frederik <60892381+frederikme@users.noreply.github.com>
2020-11-23 10:20:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import *
+from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,11 +26,8 @@ class YouTube:
 
     def openPlayList(self, url):
         self.browser.get(url)
-        self.dismissOverlays(start_delay=15)
         thread = threading.Timer(10.0, self.dismissOverlays)
-        thread.daemon = True
         thread.start()
-
 
     def search(self, query):
         words = query.split(" ")
@@ -39,46 +38,85 @@ class YouTube:
         url = "https://www.youtube.com/results?search_query={}".format(true_query[1:])
         self.browser.get(url)
         time.sleep(3)
+        threading.Timer(10.0, self.dismissOverlays).start()
+
+    first_run = True
 
     def openVideo(self, value, sleep):
-        print("opening video")
         xpath = '//*[@id="video-title"]'
-        WebDriverWait(self.browser, self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-        elements = self.browser.find_elements_by_xpath(xpath)[1:]
-        #random.shuffle(elements)
+        if self.first_run:
+            elements = self.browser.find_elements_by_xpath(xpath)
+            self.first_run = False
+        else:
+            elements = self.browser.find_elements_by_xpath(xpath)[2:]
+
+        for _ in range(4):
+            print(len(elements))
+            if len(elements) == 0:
+                time.sleep(10)
+                elements = self.browser.find_elements_by_xpath(xpath)
+
+        random.shuffle(elements)
         for element in elements:
+
             label = element.get_attribute("aria-label")
+
             if not label:
+                print("No label")
                 continue
 
-            print(label)
             if value in label:
-                time.sleep(sleep)
+                location = element.location
+                size = element.size
+                if location["x"] == 0:
+                    print("Location 0")
+                    continue
+
+                print(location)
+                print(size)
+
                 try:
-                    element.click()
-                except:
-                    return self.openVideo(value, 0)
-
-                parts = label.split(' ')
-                minutes, seconds = 0, 0
-                for index, part in enumerate(parts):
+                    sleep -= 20
+                    if sleep < 0:
+                        sleep = 0
+                    print("sleeping for {}".format(sleep))
+                    time.sleep(sleep)
+                    print("Will click " + label)
                     try:
-                        if 'sec' in part:
-                            seconds += int(part[index-1])
-                        if 'min' in part:
-                            minutes += int(part[index-1])
-                    except Exception as e:
-                        print(e)
+                        element.click()
+                    except:
+                        # there is some kind of popup
+                        time.sleep(12)
+                        try:
+                            element.click()
+                        except ElementClickInterceptedException:
+                            continue
 
-                # Sleep length of video
-                new_sleep = (minutes*60) + seconds
+                except Exception as e:
+                    print(e)
+                    exit(1)
 
-                return self.openVideo(value, new_sleep)
+                sleep = 0
 
-        print("Ending videos")
-        return
+                try:
+                    minute_text = label.split(' minute')[-2].split(' ')[-1]
+                    sleep += int(minute_text) * 60
+                except:
+                    pass
+
+                try:
+                    second_text = label.split(' second')[-2].split(' ')[-1]
+                    sleep += int(second_text)
+                except:
+                    pass
+
+                return True, sleep
+
+        return False, sleep
 
     def dismissOverlays(self, start_delay=3):
+        print("Dismissing")
+
         try:
             xpath_deny = '//*[@id="dismiss-button"]'
             WebDriverWait(self.browser, start_delay).until(EC.presence_of_element_located((By.XPATH, xpath_deny)))
@@ -96,6 +134,8 @@ class YouTube:
             WebDriverWait(self.browser, start_delay).until(EC.presence_of_element_located((By.XPATH, xpath_acceptcookies)))
             btn_accept = self.browser.find_element_by_xpath(xpath_acceptcookies)
             btn_accept.click()
+            self.browser.switch_to.default_content()
+
         except Exception as e:
             pass
 
@@ -108,43 +148,92 @@ class YouTube:
         except:
             pass
 
-if __name__ == '__main__':
-    playlist_url = ""
-    length_playlist = 11 * 3 * 60
-    amount_browsers = 7
+def startSearch():
+    while True:
+        search_query = ""
 
-    sessions = []
-
-    for index in range(amount_browsers):
         session = YouTube()
-        session.openPlayList(playlist_url)
-        sessions.append(session)
-        time.sleep(20)
+        session.search(search_query)
+
+        start = time.time()
+        max_duration = 25 * 60  # 25 minuten
+
+        sleep = 0
+
+        while True:
+            try:
+                boolean, sleep = session.openVideo(value='Teeti', sleep=sleep)
+                time.sleep(5)
+                if not boolean:
+                    print("Session openvideo returned false")
+                    # so wait till video hs been watched before going away
+                    time.sleep(sleep)
+                    break
+                print("Succesfully watched a video")
+                now = time.time()
+                # check if longer online than necessary
+                if int(now - start) > max_duration:
+                    print("time exceeded")
+                    break
+            except:
+                break
+
+        session.browser.quit()
+        print("Need a new stream")
+
+
+if __name__ == '__main__':
+
+    '''amount_browsers = 7
+
+    for _ in range(amount_browsers):
+        threading.Thread(target=startSearch).start()
+        time.sleep(35)
 
     while True:
+        pass
 
-        time.sleep(length_playlist)
+    exit(0)'''
 
-        sessions_copy = sessions.copy()
+    playlist_url = "" #or videourl"
+    length_playlist = 3 * 60 # 3 minutes
+    amount_browsers = 8
 
-        for session in sessions_copy:
-            sessions.remove(session)
-            try:
-                session.browser.quit()
-            except:
-                pass
-            session = YouTube()
-            session.openPlayList(playlist_url)
-            sessions.append(session)
-            time.sleep(20)
+    while True:
+        sessions = []
 
+        try:
+            for index in range(amount_browsers):
+                try:
+                    # sometimes crashes when connection to internet is lost
+                    session = YouTube()
+                except:
+                    continue
+                session.openPlayList(playlist_url)
+                sessions.append(session)
+                time.sleep(20)
 
+            while True:
 
-    '''
-    session = YouTube()
-    session.search("short video")
-    session.openVideo(value='', sleep=0)
-    '''
+                time.sleep(length_playlist)
+
+                sessions_copy = sessions.copy()
+
+                for session in sessions_copy:
+                    sessions.remove(session)
+                    try:
+                        session.browser.quit()
+                    except:
+                        pass
+                    session = YouTube()
+                    session.openPlayList(playlist_url)
+                    sessions.append(session)
+                    time.sleep(20)
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(60)
 
 
 
